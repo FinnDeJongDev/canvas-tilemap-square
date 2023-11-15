580c9bc41426fd961118a0b1e6402eba345f0c6e
FinnDeJongDev <finn.dejong.dev@gmail.com>
2023-11-16 00:32:25 +0100

    added tilemap

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,25 @@ const mapSettings = {
   },
   rows: 28,
   columns: 14,
+  tileMap: [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  ],
+  getTile: function (col, row) {
+    return this.tileMap[row * this.rows + col]
+  }
 }
 
 function App() {
@@ -40,11 +59,26 @@ function App() {
 
 function drawGrid(ref) {
   const ctx = ref.current.getContext('2d');
-  for (let a = 0; a < mapSettings.columns; a++) {
-    for (let b = 0; b < mapSettings.rows; b++) {
-      console.log(a, b)
+  for (let a = 0; a < mapSettings.rows; a++) {
+    for (let b = 0; b < mapSettings.columns; b++) {
+      let tile = mapSettings.getTile(a, b)
       ctx.strokeStyle = "black";
-      ctx.strokeRect(mapSettings.tile.width * b, mapSettings.tile.height * a, mapSettings.tile.width, mapSettings.tile.height)
+      ctx.strokeRect(
+        mapSettings.tile.width * a,
+        mapSettings.tile.height * b,
+        mapSettings.tile.width,
+        mapSettings.tile.height
+      )
+
+      if (tile === 1) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(
+          mapSettings.tile.width * a,
+          mapSettings.tile.height * b,
+          mapSettings.tile.width,
+          mapSettings.tile.height
+        )
+      }
     }
   }
 }
